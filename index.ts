1f282a599056577ff01433827d5831121375672c
Colin <colin_teahan@yahoo.com>
2024-04-27 15:17:43 -0700

    made into a website

--- index.ts
+++ index.ts
@@ -1,15 +1,31 @@
-import { nPrimes } from './src/nPrimes'
-import { nPrimeCounter } from './src/nPrimeCounter'
-import { promptInput } from './src/promptInput'
+import { Elysia } from "elysia";
+import { staticPlugin } from '@elysiajs/static'
+import { renderToReadableStream } from 'react-dom/server.browser'
+import { createElement } from "react";
+import App from './src/App'
 
+// bundle client side code
+await Bun.build({
+  entrypoints: ['./src/html/index.tsx'],
+  outdir: './public',
+});
 
-const n = promptInput()
-console.time()
+const app = new Elysia()
+  .use(staticPlugin())
+  .get('/', async () => {
 
-// Product Primes Method
-// console.log(...nPrimes(n))
+    // create our react App component
+    const app = createElement(App)
 
-// Prime Counter Methods
-nPrimeCounter(Number(n))
+    // render the app component to a readable stream
+    const stream = await renderToReadableStream(app, {
+      bootstrapScripts: ['/public/index.js']
+    })
 
-console.timeEnd()
+    // output the stream as the response
+    return new Response(stream, {
+      headers: { 'Content-Type': 'text/html' }
+    })
+  })
+  .onStart(() => console.log('Server started on http://localhost:3000'))
+  .listen(3000)
